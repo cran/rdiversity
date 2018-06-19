562a53188eb7ec9ae952a153ea4ae03bb8c07dbb
Sonia Mitchell <s.mitchell.2@research.gla.ac.uk>
2018-06-19 13:53:02 +0000

    version 1.2.1

--- R/check_phypartition.R
+++ R/check_phypartition.R
@@ -18,23 +18,27 @@
 check_phypartition <- function(tip_labels, partition){
   partition <- check_partition(partition)
   
-  # Remove species from partition, if not in phylogeny
+  if(is.null(row.names(partition)))
+    stop(paste("Partition object most have row names.",
+                "\nThese should match some or all of the tip labels in theh phylogeny"))
+  
+  # Remove species from the partition that aren't in the phylogeny
   if (any(!row.names(partition) %in% tip_labels)) {
     absent <- row.names(partition)[!row.names(partition) %in% tip_labels]
-    if(length(absent)==1)
-      warning(paste(absent, "is absent from the phylogeny.")) else
-        warning(paste(absent, "are absent from the phylogeny."))
+    warning(paste("The following species are not in the phylogeny:",
+                  paste0(absent, collapse=","),
+                  "\nThey have been removed from the partition."))
     partition <- partition[-which(row.names(partition) %in% absent),]
     partition <- partition / sum(partition)
   }
   
-  # Add species to partition, if in phylogeny
+  # Add species to the partition that are in the phylogeny
   if (any(!tip_labels %in% row.names(partition))) {
     absent <- tip_labels[!tip_labels %in% row.names(partition)]
-    if(length(absent)==1)
-      warning(paste(absent, "is absent from the partition.")) else
-        warning(paste(absent, "are absent from the partition."))
-    missing <- lapply(absent, function(x) matrix(rep(0, ncol(partition)), 
+    warning(paste("The following species are not in the partition:",
+                  paste0(absent, collapse=","),
+                  "\nThey have been added to the partition as empty rows."))
+    missing <- lapply(absent, function(x) matrix(rep(0, ncol(partition)),
                                                  nrow = 1))
     missing <- do.call(rbind.data.frame, missing)
     row.names(missing) <- absent
